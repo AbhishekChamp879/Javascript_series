ea45764ce0e4a41cb0a2c7a89e22cabb72d587d2
Abhishek Kumar Giri <167849920+AbhishekChamp879@users.noreply.github.com>
2024-09-11 13:21:45 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,28 @@
+ "use strict" // treat all the js code for the newer version
+
+console.log(3
+    +
+    3) // code readibility should be high so proper indentation is must 
+
+console.log("hello");
+
+let userName = "Abhishek"; // string 
+let userId = 561032; // number 
+let state; // undefined (variable assign hai but usko value nhi dia hai)
+
+// Different types of datatypes
+
+// number => 2 to power 53
+// bigInt
+// string => ""(double quotes) or ''(single quotes)
+// boolean => true/false 
+// null => standalone value hai (empty) 
+// undefined => mean abhi value assign nhi hua hai
+// unique => unique
+
+
+// object 
+
+console.log(typeof" height");
+console.log(typeof null); // type ----> objects
+console.log(typeof undefined); // type ----> undefined
