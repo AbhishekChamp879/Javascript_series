09381ff34d07f5ac6b76b5b881d1e328316b15f2
Abhishek Kumar Giri <167849920+AbhishekChamp879@users.noreply.github.com>
2024-09-12 15:17:56 +0000

    datatypes summary

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -26,3 +26,41 @@ let state; // undefined (variable assign hai but usko value nhi dia hai)
 console.log(typeof" height");
 console.log(typeof null); // type ----> objects
 console.log(typeof undefined); // type ----> undefined
+
+
+
+// -----------------------------------Datatypes Summmary-------------------------------------------
+
+// Primitive 
+//  7 tyeps : String, Number, Boolean, null, undefined, symbol, BigInt;
+
+// Examples of datatypes
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
+
+// Reference (Non primitive)
+// Array, Objects, Functions;
+
+const fruit = ["Apple", "Bananas", "Mangoes"];  // Example of Array type
+let myObj = {
+    name: "Abhishek",
+    age: 23,
+    state: "WB",
+}  // Example of object type
+
+const myFunction = function(){
+    console.log("Hello world;");
+}
+
+
+console.log(typeof fruit);  // By using typeof we can get to know the type of datatype 
